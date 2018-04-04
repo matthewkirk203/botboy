db9e960f78a9cb0b3871c5ceec6b10fe0f3c31f3
Jesse Inouye <jessinouye@gmail.com>
2018-04-03 19:00:17 -0700

    added rock, paper, scissors

--- botboy/botboy.py
+++ botboy/botboy.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 
 TOKEN = 'NDMwNTU3MTAxNDU1MDQ4NzE2.DaR7XQ.A_K3I6ULvana5W32H312GdBnZ2A'
@@ -24,6 +25,20 @@ async def hello():
 async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
+
+
+@bot.command()
+async def rpc(player_choice):
+	result_matrix = [['draw','user','bot'],['bot','draw','user'],['user','bot','draw']]
+	choices = ['rock','paper','scissors']
+	bot_choice_index = random.randint(0,2)	
+	player_choice_index = choices.index('player_choice')
+
+	result = result_matrix[bot_choice_index][player_choice_index]
+
+	await bot.say("The winner is: " + result)
+
+
 
 bot.run(TOKEN)
 
