b0e0d5b07fa5216e197142bed6591afc183f9b15
Matthew Kirk <matthewkirk203@gmail.com>
2018-04-03 20:59:44 -0600

    ow_add. def needs error checking

--- botboy/botboy.py
+++ botboy/botboy.py
@@ -1,6 +1,14 @@
-import discord
 import random
+import discord
 from discord.ext import commands
+import sqlite3 as sql
+
+# Establish db connection
+db = 'botboy_database.sqlite'
+conn = sql.connect(db)
+c = conn.cursor()
+overwatch_table = "Overwatch"
+rps_table = "RPS"
 
 TOKEN = 'NDMwNTU3MTAxNDU1MDQ4NzE2.DaR7XQ.A_K3I6ULvana5W32H312GdBnZ2A'
 
@@ -12,6 +20,7 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
+    print("DB: " +db)
     print('------')
 
 
@@ -26,7 +35,13 @@ async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
 
+@bot.command()
+async def leave():
+	conn.close()
+	await bot.say("Bye!")
+	quit()
 
+# Rock Paper Scissors
 @bot.command()
 async def rpc(player_choice):
 	result_matrix = [['draw','user','bot'],['bot','draw','user'],['user','bot','draw']]
@@ -38,7 +53,18 @@ async def rpc(player_choice):
 
 	await bot.say("The winner is: " + result)
 
+# Overwatch
+@bot.command(pass_context=True)
+async def ow_add(ctx, battle_tag, member: discord.Member = None):
+	if member is None:
+		member = ctx.message.author
+	query = "INSERT INTO " + overwatch_table + " VALUES('" + battle_tag + "', '" + str(member) + "')"
+	#print(query)
+	c.execute(query)
+	await bot.say("Added " + battle_tag + " with discord member " + str(member))
+	conn.commit()
+
 
 
 bot.run(TOKEN)
-
+conn.close()
