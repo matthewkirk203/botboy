a12f938aeead5c2775984d8c4dd2dbc21007c9bd
Jesse Inouye <jessinouye@gmail.com>
2018-04-17 22:31:21 -0700

    Added log file handling - currently off

--- botboy/setup.py
+++ botboy/setup.py
@@ -8,8 +8,15 @@ def setup_logger():
         fmt_str = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
         formatter = logging.Formatter(fmt_str)
 
+        if False:
+            log_path = 'logs/botlogs.log'
+            fh = logging.FileHandler(log_path)
+            fh.setLevel(logging.DEBUG)
+            fh.setFormatter(formatter)
+
         sh = logging.StreamHandler()
         sh.setLevel(logging.INFO)
         sh.setFormatter(formatter)
 
         logger.addHandler(sh)
+        logger.addHandler(fh)
