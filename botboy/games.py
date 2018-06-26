c44b45df2878ab8341fdf916a7c34a9a65ba5b7e
Matthew Kirk <matthewkirk203@gmail.com>
2018-06-26 14:15:32 -0400

    Added the rules to Idiot
    Probably need to clean it up/reword/clarify

--- botboy/games.py
+++ botboy/games.py
@@ -2,9 +2,9 @@ import asyncio
 import random
 
 class Game:
-	self.number_of_players = 0
-	def reset(self):
-		"reset all things to default"
+    self.number_of_players = 0
+    def reset(self):
+        "reset all things to default"
 
 	def play(self):
 		"setup game. Print to channel and so forth"
@@ -18,14 +18,16 @@ class Game:
 		
 class Player:
 	"""This player can play many types of games"""
-	def __init__(self, name, id):
-		self.hand = list()
+	def __init__(self, name, id, hand=list()):
+		self.hand = hand
 		self.name = name
 		self.id = id
 		
-	def set_hand(self, hand):
-		self.hand = hand
-		sorted(self.hand)
+	def __str__(self):
+		return "Name: {}\nID: {}\nHand: {}"format(self.name, self.id, self.hand)
+		
+	def add_to_hand(cards):
+        self.hand += cards
 	
 	def send_hand(self):
 		"""Send a DM to the user of their hand.
@@ -52,8 +54,8 @@ class TexasHoldEm(CardGame):
 	def call(self, player):
 		"match the current bet for the player that issued this command"
 
-	def raise_otherword(self, player, amount):
-		"raise the bet for the player by amount"
+	def raise_bet(self, player, amount):
+		"raise the bet for the player by amount. raise is a keyword, so raise_bet is used"
 
 	def check(self, player):
 		"basically a pass"
@@ -61,9 +63,66 @@ class TexasHoldEm(CardGame):
 class Idiot(CardGame):
 	def __init__(self,number_of_decks=1):
 		CardGame(self,number_of_decks)
-		
 	
+	@staticmethod
+	def rules():
+		"""Returns the rules for the game."""
+		rules = """Idiot is a game played with an normal deck of cards. 
+		The goal is to get rid of all of your cards. There are no winners,
+		there is only one loser, which is the idiot. Normally, we have the 
+		idiot wear a bucket on their head during the next game, but instead 
+		we'll give out a special Idiot role and keep track of losses. 
 		
+		Each player begins the game with 9 cards: 3 face down, 3 face up, and 3 for the hand.
+		The face down cards remain face down for the majority of the game, 
+		they are only flipped up once you have no other cards to play.
+		The 6 known cards dealt to each player (3 face up and 3 to the hand) can be chosen 
+		as the face-up cards for the game. You can only make these swaps at
+		the beginning of the game.
+		
+		Play proceeds as follows:
+		Each turn, a player must either play cards or pick up the pile.
+		There is no 'passing'. To play a card, you must have a card that meets or beats
+		whatever the top card on the pile is. You can play any number of the same card.
+		(e.g. Top card is a 4, I can play 2 additional 4s, a 5, 3 6s, etc.).
+		If you can't play a card (or choose not to), you must pick up the pile and
+		add it to your hand. If you pick up the pile, that is your turn.
+		At the end of your turn, you must draw cards from the deck
+		until you have at least 3 cards. If you have more than 3 cards, you do not draw.
+		Once the deck is gone, card drawing stops. At this point, cards are only added
+		to your hand via picking up the pile.
+		
+		Once your hand is empty, you can start playing your face-up cards. If you can't
+		play any of your face up cards, you must pick up the pile and cannot resume playing
+		face up cards until your hand is empty once again.
+		
+		Once your hand is empty and your face-up cards have been played, you can start
+		playing your face-down cards. These cards are revealed one at a time when they
+		are played. If the revealed card cannot be played, you must pick up the pile,
+		adding the revealed card to your hand as well. If the card cannot be played,
+		you are not required to reveal its value to the other players. If you 
+		were able to play your revealed card, that is your turn and play continues 
+		as normal. You may only play cards on the table if you have no cards in
+		your hand. The last player with cards is the IDIOT.
+		
+		Cards follow a typical ranking (A > J > 5 > 3 etc.), but has some notable exceptions:
+		2: Beats everything, beat by everything. Can always be played, can always be beaten.
+		10: Beats everything and 'blows up the pile' (removes the cards in the pile, including
+		the 10, from play). This then affords the player of the 10 an extra play (to set the pile).
+		
+		Notable additional rule: Playing four-of-a-kind also blows up the pile, following the
+		same rules as the 10, the only exception being that the value of the card must be able to 
+		be played. If playing with multiple decks, playing more than four of a kind has the same 
+		effect.
+		
+		Note:If playing with >4 players, it's wise to play with 2 decks. In general, you should
+		use ceil(num_players/2) decks. e.g: 3 players - 1 deck, 5 players - 2 decks, 9 players - 3 decks"""
+		
+		return rules
+
+if __name__ == "__main__"():
+    p = Player("Matt","1")
+    
 	
 
 
